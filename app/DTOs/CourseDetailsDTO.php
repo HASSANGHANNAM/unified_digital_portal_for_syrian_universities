931be1799d9aa10c94a43b1573b5f63e3807aa0d
HASSANGHANNAM <ite.hassang@gmail.com>
2026-08-06 00:52:15 +0300

    some fixes DTO

--- app/DTOs/CourseDetailsDTO.php
+++ app/DTOs/CourseDetailsDTO.php
@@ -9,7 +9,7 @@ class CourseDetailsDTO
     public function __construct(
         public int $id,
         public string $code,
-        public string $course_name,
+        public string $name,
         public int $credits,
         public int $college_id,
         public int $department_id,
@@ -33,7 +33,7 @@ class CourseDetailsDTO
         return new self(
             (int) $model->id,
             (string) $model->code,
-            (string) $model->course_name,
+            (string) $model->name,
             (int) $model->credits,
             (int) $model->college_id,
             (int) $model->department_id,
@@ -46,7 +46,7 @@ class CourseDetailsDTO
         return [
             'id' => $this->id,
             'code' => $this->code,
-            'course_name' => $this->course_name,
+            'name' => $this->name,
             'credits' => $this->credits,
             'college_id' => $this->college_id,
             'department_id' => $this->department_id,
